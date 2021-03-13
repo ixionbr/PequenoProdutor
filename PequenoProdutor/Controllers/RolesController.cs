8f9e3fa33818b8bd83213430ef73bff69172e73c
JOHANNES VIEIRA <johannes.vieira@fatec.sp.gov.br>
2021-03-13 13:28:18 -0300

    Adicionar arquivos de projeto.

---
+++ PequenoProdutor/Controllers/RolesController.cs
@@ -0,0 +1,120 @@
+﻿using PequenoProdutor.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuscaAnimal1.Controllers
+{
+    //[Authorize]
+    public class RolesController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var Roles = db.Roles.ToList();
+            return View(Roles);
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IdentityRole Role)
+        {
+            try
+            {
+                db.Roles.Add(Role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(Role);
+            }
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(IdentityRole Role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Role);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            IdentityRole role = db.Roles.Find(id);
+            db.Roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
